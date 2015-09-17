2d6df7cd2a00620a4d3acbdcc044f0ef7afa5d74
S. Garrido <sgarrido2011@gmail.com>
2015-09-17 12:18:22 +0200

    fix reading options file bug in BundlerApp

--- src/BundlerApp.cpp
+++ src/BundlerApp.cpp
@@ -689,7 +689,8 @@ void BundlerApp::ProcessOptions(int argc, char **argv)
 		}
 
 		char *opt_str = new char[4096];
-		fread(opt_str, 1, 4096, f);
+        int nbytes = fread(opt_str, 1, 4096, f);
+        if(nbytes < 4096) opt_str[nbytes] = '\0';
 		fclose(f);
 
                 std::string str(opt_str);
